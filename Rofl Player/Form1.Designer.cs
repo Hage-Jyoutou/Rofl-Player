87e694236dce5eb25280a050f6ff1be5a5dd04aa
hage-jyoutou <ruins_revival_wr7@yahoo.co.jp>
2018-04-13 11:13:49 +0900

    LOL_Rofl_PlayerV1.0 公開

---
+++ Rofl Player/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Rofl_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.SplitContainer splitContainer1;
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(splitContainer1)).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            splitContainer1.IsSplitterFixed = true;
+            splitContainer1.Location = new System.Drawing.Point(0, 0);
+            splitContainer1.Margin = new System.Windows.Forms.Padding(5);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(this.label3);
+            splitContainer1.Panel1.Controls.Add(this.label1);
+            splitContainer1.Panel1.Controls.Add(this.textBox1);
+            splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(this.label4);
+            splitContainer1.Panel2.Controls.Add(this.button3);
+            splitContainer1.Panel2.Controls.Add(this.label2);
+            splitContainer1.Panel2.Controls.Add(this.button2);
+            splitContainer1.Panel2.Margin = new System.Windows.Forms.Padding(5);
+            splitContainer1.Size = new System.Drawing.Size(343, 186);
+            splitContainer1.SplitterDistance = 82;
+            splitContainer1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(111, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "League of Legends　実行ファイルパス(exe)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ファイルを開く";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(111, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(319, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "リプレイ再生";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.DragDrop += new System.Windows.Forms.DragEventHandler(this.Drop);
+            this.button3.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragEnter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "リプレイファイル";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "リプレイを開く";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 186);
+            this.Controls.Add(splitContainer1);
+            this.Name = "Form1";
+            this.Text = "RoflPlayer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(splitContainer1)).EndInit();
+            splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Rofl Player/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rofl_Player
+{
+    public partial class Form1 : Form
+    {
+        string ClientPath;
+        string ReplayPath;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //コントロールをすべて取得して、ドラッグドロップのイベントを設定//
+            List<Control> controls = GetAllControls<Control>(this);
+            foreach (Control c in Controls)
+            {
+                controls.Add(c);
+            }
+
+            for(int i =0; i < controls.Count(); i++)
+            {
+                controls[i].AllowDrop = true;
+                controls[i].DragEnter += new DragEventHandler(DragEnter);
+                controls[i].DragDrop += new DragEventHandler(Drop);
+
+
+            }
+
+
+            //設定ファイルからクライアントのファイルパスがあれば読み込んで設定する//
+            if(Properties.Settings.Default.ClientDir != "")
+            {
+                if (System.IO.File.Exists(Properties.Settings.Default.ClientDir))
+                {
+                    ClientPath = Properties.Settings.Default.ClientDir;
+                    textBox1.Text = ClientPath;
+                    label3.Text = "◎";
+                }
+            }
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+        }
+
+        //フォーム上のコントロールを取得//
+        public static List<T> GetAllControls<T>(Control top) where T : Control
+        {
+            List<T> buf = new List<T>();
+            foreach (Control ctrl in top.Controls)
+            {
+                if (ctrl is T) buf.Add((T)ctrl);
+                buf.AddRange(GetAllControls<T>(ctrl));
+            }
+            return buf;
+        }
+
+
+        //LOLクライアント実行ファイルを開く
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ファイルを開くダイアログを表示//
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Properties.Settings.Default.releases;
+
+            string tmpPath = "null";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、tmpPathにファイルパスを一時格納//
+                tmpPath = ofd.FileName;
+
+                //ファイルがLOLClientであればClientPathにファイルパスを格納//
+                //違うのであればnullを格納し、警告文を表示//
+                if (System.IO.Path.GetFileName(tmpPath) == @"League of Legends.exe")
+                {
+                    ClientPath = tmpPath;
+                    textBox1.Text = tmpPath;
+                    label3.Text = "◎";
+
+                    string tmp = tmpPath.Substring(0, tmpPath.LastIndexOf("releases"));
+
+                    Properties.Settings.Default.releases = tmp;
+                    Properties.Settings.Default.ClientDir = tmpPath;
+                    Properties.Settings.Default.Save();
+                }
+                else
+                {
+                    ClientPath = "null";
+                    textBox1.Text = "LOLクライアントを選択してください";
+                    label3.Text = "✕";
+                }
+            }
+        }
+
+
+        //roflファイルを開く
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Properties.Settings.Default.ReplayDir;
+            string tmpPath = "null";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、tmpPathにファイルパスを一時格納//
+                tmpPath = ofd.FileName;
+
+                //roflファイルであればReplayPathにファイルパスを格納//
+                //違うのであればnullを格納し、警告文を表示//
+                if (System.IO.Path.GetExtension(tmpPath) == @".rofl")
+                {
+                    ReplayPath = tmpPath;
+                    label2.Text = System.IO.Path.GetFileName(tmpPath);
+                    label4.Text = "◎";
+
+                    string tmp = tmpPath.Substring(0, tmpPath.LastIndexOf(@"\"));
+                    Properties.Settings.Default.ReplayDir = tmp;
+                    Properties.Settings.Default.Save();
+                }
+                else
+                {
+                    ReplayPath = "null";
+                    label2.Text = "roflファイルを選択してね";
+                    label4.Text = "✕";
+                }
+            }
+        }
+
+        void button3_Click(object sender, EventArgs e)
+        {
+
+            if (label4.Text == "◎" & label3.Text == "◎")
+            {
+                //ショートカットを作成する//
+
+                string shortcutPath = System.IO.Directory.GetCurrentDirectory() + "\\" + "test.lnk";
+                IWshRuntimeLibrary.WshShell shell = new IWshRuntimeLibrary.WshShell();
+                IWshRuntimeLibrary.IWshShortcut shortcut = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(shortcutPath); //ショートカットのパスを指定して、WshShortcutを作成
+
+                shortcut.TargetPath = ClientPath;               //リンク先
+                shortcut.Arguments = "\"" + ReplayPath + "\"";  //コマンドパラメータ を設定
+                shortcut.WorkingDirectory = System.IO.Path.GetDirectoryName(ClientPath);    //作業ディレクトリを設定
+                shortcut.Save();                                                            //ショートカットを作成
+                System.Runtime.InteropServices.Marshal.FinalReleaseComObject(shortcut);     //後処理
+                System.Runtime.InteropServices.Marshal.FinalReleaseComObject(shell);
+
+                var proc = new Process(); //新しいプロセスを作成
+                proc.StartInfo.FileName = shortcutPath; // ショートカットパスをセット
+                proc.Start(); //　プロセスをスタート
+                proc.WaitForExit(); //プロセスが終了するまで待機
+
+                //ショートカットを消去する//
+                File.Delete(shortcutPath);
+            }
+        }
+
+
+        //ドラッグを受け入れる//
+        private new void DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        //ドロップされたものがroflファイルならReplayPathにファイルパスを格納//
+        private void Drop(object sender,System.Windows.Forms.DragEventArgs e)
+        {
+            string[] tmpPath = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            if (System.IO.Path.GetExtension(tmpPath[0]) == @".rofl")
+            {
+                ReplayPath = tmpPath[0];
+                label2.Text = System.IO.Path.GetFileName(tmpPath[0]);
+                label4.Text = "◎";
+            }
+            else
+            {
+                ReplayPath = "null";
+                label2.Text = "roflファイルを選択してね";
+                label4.Text = "✕";
+            }
+        }
+    }
+}
